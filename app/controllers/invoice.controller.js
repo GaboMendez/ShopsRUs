20b361bccf870091adea04b80d83e931da00fd15
GabrielMendez <gabriel.mendez0@hotmail.com>
2022-03-14 19:12:17 -0400

    Validations for creating invoice with products

--- app/controllers/invoice.controller.js
+++ app/controllers/invoice.controller.js
@@ -3,28 +3,62 @@ const Invoice = require('../models/invoice.model.js');
 // Create and Save a new Invoice
 exports.create = (req, res) => {
   // Validate request
-  if (Object.keys(req.body).length === 0) {
+  if (!req.body.clientId || !req.body.products) {
     res.status(400).send({
-      message: 'Content can not be empty!',
+      message: 'Invalid keys properties for this request or Content is empty!',
     });
     return;
-  }
-  for (var key in req.body) {
-    if (req.body.hasOwnProperty(key)) {
-      let value = req.body[key];
-      console.log(`value for ${key} is ${value}`);
-    }
   }
   // Create a Invoice
   const invoice = new Invoice({
     clientId: req.body.clientId,
     products: req.body.products,
   });
+  // Validate products
+  if (!Array.isArray(invoice.products)) {
+    res.status(400).send({
+      message: 'You must provide an array of products',
+    });
+    return;
+  }
+  if (invoice.products.length == 0) {
+    res.status(400).send({
+      message: 'You must provide products of your purchase',
+    });
+    return;
+  }
+
+  let keyError = 0,
+    typeError = 0;
+  invoice.products.forEach((product) => {
+    const keys = Object.keys(product);
+    const values = Object.values(product);
+    if (!keys.includes('productId') || !keys.includes('quantity'))
+      productError++;
+
+    if (!values.every((item) => typeof item === 'number')) typeError++;
+  });
+  if (keyError > 0) {
+    res.status(400).send({
+      message:
+        'You must use [productId] and [quantity] properties in product object',
+    });
+    return;
+  }
+  if (typeError > 0) {
+    res.status(400).send({
+      message:
+        'You must use numeric values for [productId] and [quantity] properties in product object',
+    });
+    return;
+  }
+
   // Save Invoice in the database
   Invoice.create(invoice, (err, data) => {
     if (err)
       res.status(500).send({
-        message: err.message || 'Some error occurred while retrieving clients.',
+        message:
+          err.message || 'Some error occurred while retrieving invoices.',
       });
     else res.send(data);
   });
