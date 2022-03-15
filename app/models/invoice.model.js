af1ee7b7f3ba3b6ea9aba3be90ebd8569a6ce498
GabrielMendez <gabriel.mendez0@hotmail.com>
2022-03-15 01:07:36 -0400

    Create Invoice with descounts finish

--- app/models/invoice.model.js
+++ app/models/invoice.model.js
@@ -1,4 +1,5 @@
 const client = require('./db');
+const Client = require('../models/client.model.js');
 
 // constructor
 const Invoice = function (invoice) {
@@ -6,22 +7,42 @@ const Invoice = function (invoice) {
   this.products = invoice.products;
 };
 
-Invoice.create = (newInvoice, result) => {
-  console.log('newInvoice', newInvoice);
-  client.query(
-    'INSERT INTO invoice (client_id) VALUES ($1) RETURNING id',
-    [newInvoice.client_id],
-    (err, res) => {
-      if (err) {
-        console.log('error: ', err);
-        result(err, null);
-        return;
-      }
-      const { rows, fields } = res;
-      console.log('created Invoice: ', { id: rows[0].id, ...newInvoice });
-      result(null, { id: rows[0].id, ...newInvoice });
-    }
-  );
+Invoice.create = async (newInvoice, result) => {
+  // Get discount by client
+  let discount;
+  try {
+    discount = await getDiscount(newInvoice);
+  } catch (err) {
+    console.log(err);
+    result(err, null);
+    return;
+  }
+  // Create Invoice Details
+  let totalInvoice, finalAmount;
+  try {
+    const res = await client.query(
+      'INSERT INTO invoice (client_id) VALUES ($1) RETURNING id',
+      [newInvoice.client_id]
+    );
+    const { rows, fields } = res;
+    console.log(rows[0]);
+
+    // Insert Details and get Total price value
+    totalInvoice = await createDetail(newInvoice, rows[0].id, discount);
+    console.log('Amount without every 100$ discount', totalInvoice);
+    finalAmount = generalDiscount(totalInvoice);
+    console.log('Amount with every 100$ discount', finalAmount);
+    console.log('created Invoice: ', {
+      id: rows[0].id,
+      ...newInvoice,
+      totalAmount: finalAmount,
+    });
+    result(null, { id: rows[0].id, ...newInvoice, totalAmount: finalAmount });
+  } catch (err) {
+    console.log(err);
+    result(err, null);
+    return;
+  }
 };
 
 Invoice.getAll = (clientName, clientId, result) => {
@@ -91,6 +112,102 @@ Invoice.removeAll = (result) => {
     console.log(`deleted ${res.rowCount} invoices`);
     result(null, res);
   });
+};
+
+const getDiscount = async (newInvoice) => {
+  let discount;
+  const getClient = `SELECT type_id, created_at FROM client WHERE id = ${newInvoice.clientId}`;
+  const getDiscount = `SELECT discount.id percent_id, discount.percent percent_value, discount.type_id, type.name percent_type
+                         FROM discount INNER JOIN type ON type.id = discount.type_id`;
+
+  // get client type and created date
+  const clientRes = await client.query(getClient);
+  if (!clientRes.rows[0])
+    throw new Error(`Client with id ${newInvoice.clientId} is not found.`);
+
+  const { type_id, created_at } = clientRes.rows[0];
+  // get all discounts
+  const discountRes = await client.query(getDiscount);
+  const { percent_id, percent_value } =
+    discountRes.rows.find((row) => row.type_id == type_id) || {};
+  // Assign discount if appears
+  discount = { id: percent_id, value: percent_value };
+  // Get 2 years ago date from now...
+  const compare_date = new Date();
+  compare_date.setFullYear(compare_date.getFullYear() - 2);
+  // If Client is not Afiliado or Employee, check if have more than 2 years been client...
+  if (!discount && created_at < compare_date) {
+    const { percent_id, percent_value } =
+      discountRes.rows.find((row) =>
+        row.percent_type.toLowerCase().includes('fidelidad')
+      ) || {};
+    discount = { id: percent_id, value: percent_value };
+  }
+  return discount;
+};
+
+const createDetail = async (newInvoice, invoiceId, discount) => {
+  let totalInvoice = 0;
+  const getProduct = `SELECT product.name, product.price, category.name category_name FROM product 
+                      INNER JOIN category ON category.id = product.category_id WHERE product.id = $1`;
+  const insertDetail =
+    'INSERT INTO invoice_detail (invoice_id, discount_id, product_id, quantity, price) VALUES ($1, $2, $3, $4, $5) RETURNING id';
+
+  await asyncForEach(newInvoice.products, async (item) => {
+    // get product price
+    const productRes = await client.query(getProduct, [item.productId]);
+    if (!productRes.rows[0])
+      throw new Error(`Product with id ${item.productId} is not found.`);
+
+    const { name, price, category_name } = productRes.rows[0];
+    console.log(
+      `Product: ${name} | price = ${price} | category = ${category_name}`
+    );
+    // get discount by category
+    const discount_id = category_name.toLowerCase().includes('comestible')
+      ? undefined
+      : discount.id;
+    const discount_percent = category_name.toLowerCase().includes('comestible')
+      ? 0
+      : discount.value;
+    // get product price with quantity and discount
+    const totalPrice = price * item.quantity;
+    const discountedPrice = totalPrice - (totalPrice * discount_percent) / 100;
+    // sum value to total invoice
+    totalInvoice += discountedPrice;
+
+    const detail = await client.query(insertDetail, [
+      invoiceId,
+      discount_id,
+      item.productId,
+      item.quantity,
+      discountedPrice,
+    ]);
+    console.log('created Invoice Detail: ', {
+      detailId: detail.rows[0].id,
+      invoiceId: invoiceId,
+      discountValue: discount_percent,
+      productName: name,
+      productPrice: price,
+      quantity: item.quantity,
+      price: totalPrice,
+      discountedPrice: discountedPrice,
+    });
+  });
+
+  return totalInvoice;
+};
+
+const asyncForEach = async (array, callback) => {
+  for (let index = 0; index < array.length; index++) {
+    await callback(array[index], index, array);
+  }
+};
+
+const generalDiscount = (total) => {
+  // for every $100 discounts of $5 apply.
+  let times = Math.trunc(total / 100); //trunc() function returns only the integer portion of a number.
+  return total - times * 5;
 };
 
 module.exports = Invoice;
